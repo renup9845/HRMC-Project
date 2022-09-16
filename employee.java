897caf235a24b24140136af1e272d94ff0b9624c
renup9845 <110659991+renup9845@users.noreply.github.com>
2022-09-16 19:12:57 +0530

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,17 @@
+package com.slot2jdbc;
+
+public class employee {
+	
+	//Employee Model to Create Account
+	
+	int eid;
+	String ename;
+	String edomain;
+	String edesignation;
+	String elocation;
+	String ephone;
+	int esal;
+	
+	
+
+}
